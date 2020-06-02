6f911f7d83e9410dbbc0b7e771e84b76940ae423
hz6 <hz6@ualberta.ca>
2020-06-02 08:08:02 -0600

    test Google OAuth login

--- client/src/components/partials/header.jsx
+++ client/src/components/partials/header.jsx
@@ -24,7 +24,12 @@ export default class Header extends Component {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Login <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">
+                Login
+                <span className="sr-only">
+                  (current)
+                </span>
+              </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">My Posts</a>
