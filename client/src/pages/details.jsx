1faeece2d0869e0a8a81eda32947d761686663f1
hz6 <hz6@ualberta.ca>
2020-06-20 17:05:59 -0600

    detail

--- client/src/pages/details.jsx
+++ client/src/pages/details.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from "react";
-import {Avatar, Card, CardContent, Typography} from "@material-ui/core";
-import PostForm from "../post/postForm";
+import React, { Component } from 'react'
 
-class UserHeader extends Component {
-  
+class DetailPage extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount = async () => {
+
+  }
+
   render() {
     return (
+
       <div className="jumbotron">
         <Card>
           <CardContent>
@@ -13,29 +19,23 @@ class UserHeader extends Component {
               
               <div className="col-3">
                 <Avatar 
-                  src={this.props.currentUser.image} 
+                  src={""} 
                   style={{height:100, width:100, margin:5}} 
                 />
               </div>
               
               <div className="col-9">
                 <Typography style={{margin:5}}>
-                  Name: {this.props.currentUser.username}
-                </Typography>
-                <Typography style={{margin:5}}>
-                  Email: {this.props.currentUser.email}
+                  Name: {"Anthony Zhang"}
                 </Typography>
               </div>
             
             </div>
           </CardContent>
         </Card>
-        <div style={{marginTop:10}}>
-          <PostForm/>
-        </div>
       </div>
     )
   }
 }
 
-export default UserHeader;
+export default DetailPage
