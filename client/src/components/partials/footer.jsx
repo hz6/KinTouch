72ee124286e20b8ad266f3d4bdbd66c17bf17aec
hz6 <hz6@ualberta.ca>
2020-06-26 13:00:10 -0700

    footer

--- client/src/components/partials/footer.jsx
+++ client/src/components/partials/footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 export default () => {
   return (
-    
-      <div style={{margin:40}}>
+    <div style={{marginTop:40,marginLeft:15,marginRight:15}} className="jumbotron">
+      <h2>This is the bottom</h2>
+      <p>Refresh to get back to Top</p>
     </div>
-    
-    
   );
 }
