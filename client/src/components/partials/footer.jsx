13b997bd3052a96aa0f1f0d9d5a76d43804f3938
hz6 <hz6@ualberta.ca>
2020-06-26 13:01:06 -0700

    footer

--- client/src/components/partials/footer.jsx
+++ client/src/components/partials/footer.jsx
@@ -4,7 +4,6 @@ export default () => {
   return (
     <div style={{marginTop:40,marginLeft:15,marginRight:15}} className="jumbotron">
       <h2>This is the bottom</h2>
-      <p>Refresh to get back to Top</p>
     </div>
   );
 }
