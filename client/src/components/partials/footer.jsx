765b74655ed80d9b3a2aea8d840487b9ce5d3168
hz6 <hz6@ualberta.ca>
2020-06-28 13:40:55 -0600

    footer size

--- client/src/components/partials/footer.jsx
+++ client/src/components/partials/footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default () => {
   return (
-    <div style={{marginTop:40,marginLeft:15,marginRight:15}} className="jumbotron">
-      <h2>This is the bottom</h2>
+    <div style={{marginTop:40,marginLeft:15,marginRight:15, height:10}} className="jumbotron">
+      <h4>You've Reached The bottom</h4>
     </div>
   );
 }
