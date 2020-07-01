6d869d61d0d21e36e196102db7604d08f1dc54b3
hz6 <hz6@ualberta.ca>
2020-06-30 19:11:05 -0600

    header

--- client/src/components/partials/header.jsx
+++ client/src/components/partials/header.jsx
@@ -16,11 +16,13 @@ class Header extends Component {
         return null;
       case false:
         return (
+          <div className="row">
             <li className="nav-item active">
               <a className="nav-link" href="/auth/google">
                 Login
               </a>
             </li>
+          </div>
         );
       default:
         return (
@@ -43,19 +45,16 @@ class Header extends Component {
   render() {
     
     return (
-      <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
-        
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <a className="navbar-brand" href="/">KinTouch</a>
-        
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" ></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">{this.renderHeader()}</ul>
+        </button> */}
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">{this.renderHeader()}</ul>
         </div>
-        
       </nav>
+      
     )
   }
 }
