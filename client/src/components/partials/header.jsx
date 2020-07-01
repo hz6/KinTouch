3a6209d8711e0478f74e5544bba71f790f9e2897
hz6 <hz6@ualberta.ca>
2020-06-30 19:11:27 -0600

    header

--- client/src/components/partials/header.jsx
+++ client/src/components/partials/header.jsx
@@ -47,9 +47,9 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <a className="navbar-brand" href="/">KinTouch</a>
-        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" ></span>
-        </button> */}
+        </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">{this.renderHeader()}</ul>
         </div>
