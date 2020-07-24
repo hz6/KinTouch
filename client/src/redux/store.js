3a4945853121c23747eedc1a9aab4e6740a8db5b
hz6 <hz6@ualberta.ca>
2020-07-24 13:12:57 -0600

    card-preview deleted

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,10 @@
-import {createStore, applyMiddleware} from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./root-reducer";
-import logger from "redux-logger"
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-const middlewares = [logger]; // there will be more items like [logger, thunk, sage];
+const middlewares = [thunk, logger]; // there will be more items like [logger, thunk, sage];
 
-const store = createStore(rootReducer,applyMiddleware(...middlewares));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store
