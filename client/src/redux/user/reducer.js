7eae3eb8f5a4b0e299d2ba743a47dbaa0b0a2aa7
hz6 <hz6@ualberta.ca>
2020-07-24 08:49:47 -0600

    commit

--- client/src/redux/user/reducer.js
+++ client/src/redux/user/reducer.js
@@ -1,18 +1,18 @@
-import {UserActionTypes} from "./types"; 
+import { UserActionTypes } from "./types";
 
 // const [state, dispatch] = useReducer(reducer, initialState, init)
 const INITIAL_STATE = {
-  currentUser:null,
-  coupleName:null,
-  age:0,
+  currentUser: null,
+  coupleName: null,
+  age: 0,
 }
 
-const userReducer = (state=INITIAL_STATE, action) => {
-  switch (action.type){
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
     case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
-        currentUser:action.payload===undefined||action.payload===""? false: action.payload
+        currentUser: action.payload === undefined || action.payload === "" ? false : action.payload
       };
     default:
       return state;
