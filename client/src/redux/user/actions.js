d1517dcc617a50ccdf48695b1269a48c4f169520
hz6 <hz6@ualberta.ca>
2020-07-25 08:21:07 -0600

    remove cmmt

--- client/src/redux/user/actions.js
+++ client/src/redux/user/actions.js
@@ -1,5 +1,5 @@
 import { UserActionTypes } from "./types";
-// types.js文件里面没有 export default 所以需要 destructure {}
+
 export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user
